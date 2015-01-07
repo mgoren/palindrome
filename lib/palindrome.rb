943878eb8657115fa03244cc8fa6e98dc0427063
Debbie Cravey & Mike Goren <student@epicodus.com>
2015-01-07 15:30:35 -0800

    change method to use unshift instead of reverse

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -3,9 +3,23 @@ class String
     word = self
     palindrome = false
 
-    if word == word.reverse()
+    # reverse word easy way:
+    # word_reversed = word.reverse()
+
+    # reverse word hard way:
+    reversed_array = []
+    split_word = word.split("")
+    split_word.each() do |letter|
+      reversed_array.unshift(letter)
+    end
+    word_reversed = reversed_array.join()
+
+    #check if word matches reversed word
+    if word == word_reversed
       palindrome = true
     end
+
+    #return our boolean
     palindrome
 
   end
