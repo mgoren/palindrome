ff472946af1b79129a963a78265fa76057976271
Debbie Cravey & Mike Goren <student@epicodus.com>
2015-01-07 15:18:28 -0800

    website creation

--- app.rb
+++ app.rb
@@ -1,12 +1,22 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
-require('./lib/find_replace')
+require('./lib/palindrome')
 
 get('/form') do
   erb(:form)
 end
 
 get('/results') do
+  word = params.fetch('word')
+  result_boolean = word.palindrome()
+
+  if result_boolean
+    result_string = word + " is a palindrome!"
+  else
+    result_string = word + " is not a palindrome. Sorry."
+  end
+
+  @result = result_string
   erb(:results)
 end
